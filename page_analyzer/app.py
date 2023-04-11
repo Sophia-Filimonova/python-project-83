5f41254e48c4e02fd174670efb9187270e8a23be
Sophia Filimonova <sophfil708@gmail.com>
2023-04-11 18:15:13 +0300

    update add.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,38 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
 
-users = ['mike', 'mishel', 'adel', 'keks', 'kamila']
-
 
 @app.route('/')
 def hello_world():
     return 'Hello!'
-
-
-@app.route('/users')
-def get_users():
-    term = request.args.get('term', '')
-    filtered_users = filter(
-        lambda user: term.lower() in user, users)
-
-    return render_template(
-        'users/index.html',
-        users=filtered_users,
-        search=term,
-      )
-
-
-@app.route('/courses/<id>')
-def courses(id):
-    return f'Course id: {id}'
-
-
-@app.route('/users/<id>')
-def users_(id):
-    return render_template(
-        'users/show.html',
-        id=id,
-    )
